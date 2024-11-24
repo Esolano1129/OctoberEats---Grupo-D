7846da8582a26d9e5fca9a2fb14f48562654d86e
Esolano1129 <edgardoasolano@gmail.com>
2024-11-23 20:07:07 -0600

    Login Code

--- OctberEatsProject - Grupo D/src/main/java/org/example/Login.java
+++ OctberEatsProject - Grupo D/src/main/java/org/example/Login.java
@@ -1,5 +1,22 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Login {
+    private JPanel panel1;
+    private JTextField TxTUsername;
+    private JButton Registerbutton;
+    private JButton Loginbutton;
+    private JLabel LUsername;
+    private JLabel LPassword;
+    private JPasswordField PassFieldPassword;
+
+    private String username;
+    private String Password;
+    private int id;
+
+
+
+
 
 }
