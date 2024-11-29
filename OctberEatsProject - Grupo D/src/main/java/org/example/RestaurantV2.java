cd9e69c4fce632930a1b9c0e3370d940313cdd8f
Esolano1129 <edgardoasolano@gmail.com>
2024-11-29 10:10:41 -0600

    test

--- OctberEatsProject - Grupo D/src/main/java/org/example/RestaurantV2.java
+++ OctberEatsProject - Grupo D/src/main/java/org/example/RestaurantV2.java
@@ -1,9 +1,32 @@
 package org.example;
 
+import javax.swing.*;
+
 public class RestaurantV2 {
 
+
+    private JLabel RestNameLabel;
+    private JLabel AddressLabel;
+    private JLabel ScheduleLabel;
+    private JLabel RatingLabel;
+    private JLabel CategoryLabel;
+    private JLabel PhoneLabel;
+    private JLabel EmailLabel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTable DataJTable;
+    private JButton Createbutton;
+    private JButton DeleteButton;
+    private JButton UpdateButton;
+    private JButton RefreshListButton;
 
     public void CreateRestaurant(){
 
     }
+
 }
